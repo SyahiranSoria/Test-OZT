1467578f2eebbe400dc8dc736618b92b9b7a1d63
SyahiranSoria <syahiran.soria@gmail.com>
2024-03-25 15:54:15 +0800

    User Authenticate

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\admin as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
