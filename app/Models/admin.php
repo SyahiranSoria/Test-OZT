44179b316195479e6773bed6f2c4fa0a8cef9d7a
SyahiranSoria <syahiran.soria@gmail.com>
2024-03-25 16:29:03 +0800

    thrid commit

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
 class admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
